de4668ab7364a8ea2c66d13d8702f615e73bc7b3
Yi-Shin Shih <yihshih@cs.nctu.edu.tw>
2014-04-22 19:56:09 +0800

    fix not turned on bug

--- vimx.py
+++ vimx.py
@@ -3,24 +3,25 @@
 import subprocess
 from os.path import expanduser
 
-command = ['dialog', '--checklist', 'Choose Vim Settings:', '0', '0', '0']
+basic = ['dialog', '--checklist', 'Choose Vim Settings:', '0', '0', '0']
 opt_file = open('options')
 opts = []
 for line in opt_file:
     opts.append([str.strip(line.split(' ', 1)[0]), str.strip(line.split(' ', 1)[1])])
 i = 1
 for opt in opts:
-    command += [str(i), opt[1], 'on']
+    basic += [str(i), opt[1], 'on']
     i += 1
 #print(command)
-process = subprocess.Popen(command, stderr=subprocess.PIPE)
+process = subprocess.Popen(basic, stderr=subprocess.PIPE)
 
 results = process.communicate()[1].decode().split()
 #print(result)
 
 vimrc = open(expanduser("~")+'/.vimrc', 'w')
-for result in results:
-    print('set', opts[eval(result)-1][0], file=vimrc)
-
-
+for i in range(len(opts)+1)[1:]:
+    if str(i) in results:
+        print('set', opts[i-1][0], file=vimrc)
+    else:
+        print('set', 'no' + opts[i-1][0], file=vimrc)
 
