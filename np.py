3bf943054963c760ac255b6479bc708c7ef7eef5
Yi-Shin Shih <yihshih@cs.nctu.edu.tw>
2014-06-11 22:44:30 +0800

    testing npyscreen

---
+++ np.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import npyscreen
+
+class MainForm(npyscreen.Form):
+    def create(self):
+        self.add(npyscreen.TitleFixedText, name = "Tab:")
+        self.add(npyscreen.TitleText, name = "tabstop:", value="8")
+        self.add(npyscreen.TitleText, name = "shiftwidth:", value="8")
+        self.result = self.add(npyscreen.MultiSelect, scroll_exit=True, values = ['expandtab'])
+
+    def afterEditing(self):
+        self.parentApp.setNextForm(None)
+
+class VimXApp(npyscreen.NPSAppManaged):
+    def onStart(self):
+        self.registerForm('MAIN', MainForm())
+
+if __name__ == '__main__':
+    V = VimXApp()
+    V.run()
